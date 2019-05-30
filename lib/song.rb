8666447a10e91610bebb1158dcfa1df680a90fd4
Trish Ogen <patriciaogen@gmail.com>
2019-05-30 03:33:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,15 @@
 class Song
   attr_accessor :name
-  
+
   def initialize(name)
     @name = name
+  end
+
+  def self.new_by_filename(filename)
+    song = self.new
+    split_name = filename.split(/\s-\s|\.mp3/)
+    song.name = split_name[1]
+    song
   end
 
 end
