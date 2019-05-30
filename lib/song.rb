720041b9390673e0921579fc511576852bd5bcfb
Trish Ogen <patriciaogen@gmail.com>
2019-05-30 03:37:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,9 +6,8 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song = self.new
     split_name = filename.split(/\s-\s|\.mp3/)
-    song.name = split_name[1]
+    song = self.new(split_name[1])
     song
   end
 
