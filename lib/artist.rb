b11e01cad0d78e3a69aafa24e6d22694a2e6f380
Trish Ogen <patriciaogen@gmail.com>
2019-05-30 03:17:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    puts @@all.name
+    puts self.all.name
       @name = name
       artist = Artist.new(name)
       self.all << artist
